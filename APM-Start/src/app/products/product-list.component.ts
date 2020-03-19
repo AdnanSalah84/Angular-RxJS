b05c175de5626be0677a086701229904037f9214
Adnan Salahuddin <adnan.salah84@gmail.com>
2020-03-19 17:21:17 +0500

    Added aysnc pipe

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,41 +1,46 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Subscription } from 'rxjs';
+//import { Subscription } from "rxjs";
 
-import { Product } from './product';
-import { ProductService } from './product.service';
+import { Product } from "./product";
+import { ProductService } from "./product.service";
+import { Observable } from "rxjs";
 
 @Component({
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
-export class ProductListComponent implements OnInit, OnDestroy {
-  pageTitle = 'Product List';
-  errorMessage = '';
+//, OnDestroy
+export class ProductListComponent implements OnInit {
+  pageTitle = "Product List";
+  errorMessage = "";
   categories;
 
-  products: Product[] = [];
-  sub: Subscription;
+  //products: Product[] = [];
+  products$: Observable<Product[]>;
+  //sub: Subscription;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe(
-        products => this.products = products,
-        error => this.errorMessage = error
-      );
+    // this.sub = this.productService.getProducts()
+    //   .subscribe(
+    //     products => this.products = products,
+    //     error => this.errorMessage = error
+    //   );
+
+    this.products$ = this.productService.getProducts();
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+  // ngOnDestroy(): void {
+  //   this.sub.unsubscribe();
+  // }
 
   onAdd(): void {
-    console.log('Not yet implemented');
+    console.log("Not yet implemented");
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    console.log("Not yet implemented");
   }
 }
