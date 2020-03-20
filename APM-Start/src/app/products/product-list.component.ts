cf7cd6561163e45a1f0e1226d02f89b93184eac5
Adnan Salahuddin <adnan.salah84@gmail.com>
2020-03-20 12:32:38 +0500

    Created filter and subject/behavior subject

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -4,8 +4,16 @@ import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 
 import { Product } from "./product";
 import { ProductService } from "./product.service";
-import { Observable, of, EMPTY } from "rxjs";
-import { catchError } from "rxjs/operators";
+import {
+  Observable,
+  of,
+  EMPTY,
+  Subject,
+  combineLatest,
+  BehaviorSubject
+} from "rxjs";
+import { catchError, map, startWith } from "rxjs/operators";
+import { ProductCategoryService } from "../product-categories/product-category.service";
 
 @Component({
   templateUrl: "./product-list.component.html",
@@ -17,6 +25,9 @@ export class ProductListComponent {
   pageTitle = "Product List";
   errorMessage = "";
   categories;
+  selectedCategoryId = 1;
+  private categorySelectedSubject = new BehaviorSubject<number>(0);
+  categorySelectedAction$ = this.categorySelectedSubject.asObservable();
 
   //products: Product[] = [];
   //products$: Observable<Product[]>;
@@ -26,30 +37,67 @@ export class ProductListComponent {
   //     return EMPTY;
   //   })
   // );
-  products$ = this.productService.productWithCategory$.pipe(
+
+  //sub: Subscription;
+
+  constructor(
+    private productService: ProductService,
+    private productCategoryService: ProductCategoryService
+  ) {}
+
+  /*products$ = this.productService.productWithCategory$.pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );*/
+
+  products$ = combineLatest([
+    this.productService.productWithCategory$,
+    this.categorySelectedAction$
+  ]).pipe(
+    map(([products, selectedCategoryId]) =>
+      products.filter(product =>
+        selectedCategoryId ? product.categoryId === selectedCategoryId : true
+      )
+    ),
     catchError(err => {
       this.errorMessage = err;
       return EMPTY;
     })
   );
-  //sub: Subscription;
 
-  constructor(private productService: ProductService) {}
+  /*productsSimpleFilter$ = this.productService.productWithCategory$.pipe(
+    map(products =>
+      products.filter(product =>
+        this.selectedCategoryId
+          ? product.categoryId === this.selectedCategoryId
+          : true
+      )
+    )
+  );*/
 
-  // ngOnInit(): void {
-  //   // this.sub = this.productService.getProducts()
-  //   //   .subscribe(
-  //   //     products => this.products = products,
-  //   //     error => this.errorMessage = error
-  //   //   );
+  categories$ = this.productCategoryService.productCategories$.pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
 
-  //   this.products$ = this.productService.getProducts().pipe(
-  //     catchError(err => {
-  //       this.errorMessage = err;
-  //       return EMPTY;
-  //     })
-  //   );
-  // }
+  /*ngOnInit(): void {
+    // this.sub = this.productService.getProducts()
+    //   .subscribe(
+    //     products => this.products = products,
+    //     error => this.errorMessage = error
+    //   );
+
+    this.products$ = this.productService.getProducts().pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
+  }*/
 
   // ngOnDestroy(): void {
   //   this.sub.unsubscribe();
@@ -60,6 +108,7 @@ export class ProductListComponent {
   }
 
   onSelected(categoryId: string): void {
-    console.log("Not yet implemented");
+    //this.selectedCategoryId = +categoryId;
+    this.categorySelectedSubject.next(+categoryId);
   }
 }
